7800e2c06da224a692af1e958b30fd26c69bb9ef
zheyi613 <zheyi880613@gmail.com>
2023-08-17 14:26:04 +0800

    Modify config to decrease time of context switch

--- Core/Inc/FreeRTOSConfig.h
+++ Core/Inc/FreeRTOSConfig.h
@@ -56,10 +56,10 @@
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 100 )
 #define configTOTAL_HEAP_SIZE		( ( size_t ) ( 16 * 1024 ) )
 #define configMAX_TASK_NAME_LEN		( 20 )
-#define configUSE_TRACE_FACILITY	1
+#define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		1
-#define configUSE_MUTEXES		1
+#define configUSE_MUTEXES		0
 #define configQUEUE_REGISTRY_SIZE	8
 #define configCHECK_FOR_STACK_OVERFLOW	0
 #define configUSE_RECURSIVE_MUTEXES	0
@@ -67,6 +67,7 @@
 #define configUSE_APPLICATION_TASK_TAG	0
 #define configUSE_COUNTING_SEMAPHORES	0
 #define configGENERATE_RUN_TIME_STATS	0
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION	1
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		0
